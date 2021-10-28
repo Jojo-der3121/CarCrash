79c9937bd29d6f905bc6e42477b125f3cd4d719c
Jojoder-3121 <johannes.g.brandau@gmail.com>
2021-10-28 16:07:13 +0200

    added file to project

---
+++ Carcrash/Game/ObjectSizeAndLocation.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Carcrash.Game
+{
+    class ObjectSizeAndLocation
+    {
+        public List<int> CollisionDimensions { get; set; }
+        public int Top { get; set; }
+        public int Left { get; set; }
+    }
+}
